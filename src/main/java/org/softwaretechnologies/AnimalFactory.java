5c8c90212d76ed393f663f5aaf32b6d27bb6a28b
w2lvera <w2lvera@gmail.com>
2023-09-29 00:24:14 +0300

    task4

---
+++ src/main/java/org/softwaretechnologies/AnimalFactory.java
@@ -0,0 +1,28 @@
+package org.softwaretechnologies;
+
+import org.softwaretechnologies.animals.*;
+
+public class AnimalFactory {
+
+    /**
+     * Метод для создания животных по типу
+     * @param name - имя животного
+     * @param type - тип животного
+     * @return животное, соответствующее каждому из типов.
+     */
+
+
+    public static Animal createAnimal(String name, AnimalType type) {
+        /* TODO в зависимости от type создайте и верните нужное животное */
+            switch (type){
+                case CAT:
+                    return new Cat(name);
+                case DOG:
+                    return new Dog(name);
+                case COW:
+                    return new Cow(name);
+                default:
+                    throw new IllegalArgumentException("Unknown animal type: " + type);
+            }
+    }
+}
